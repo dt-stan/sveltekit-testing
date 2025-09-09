7e4bba70049c1f2be13d7a081971e008c8c5c287
Switalski, Stanley <stanley.switalski@dynatrace.com>
2025-09-09 01:16:57 -0400

    feat: first attempt to implement integrated observability

---
+++ src/instrumentation.server.ts
@@ -0,0 +1,57 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { createAddHookMessageChannel } from 'import-in-the-middle';
+import { register } from 'module';
+
+// Francois Commit
+import {
+    OTEL_SERVICE_NAME,
+    OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
+    OTEL_EXPORTER_OTLP_ENDPOINT,
+    OTLP_AUTH_HEADER,
+    OTEL_EXPORTER_OTLP_HEADERS,
+    OTEL_DIAGNOSTICS
+} from '$env/static/private';
+
+const { registerOptions } = createAddHookMessageChannel();
+register('import-in-the-middle/hook.mjs', import.meta.url, registerOptions);
+
+console.log("--------------------------------");
+console.log({
+OTEL_SERVICE_NAME,
+OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
+OTEL_EXPORTER_OTLP_ENDPOINT,
+OTLP_AUTH_HEADER,
+OTEL_EXPORTER_OTLP_HEADERS,
+OTEL_DIAGNOSTICS
+});
+console.log("--------------------------------");
+
+const resource = resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: OTEL_SERVICE_NAME ?? 'sveltekit-testing-intobs'
+});
+
+const sdk = new NodeSDK({
+	resource,
+	traceExporter: new OTLPTraceExporter({
+        // e.g. https://your-otlp.example.com/v1/traces
+        url: OTEL_EXPORTER_OTLP_TRACES_ENDPOINT
+             ?? OTEL_EXPORTER_OTLP_ENDPOINT
+             ?? undefined,
+        headers: {
+            Authorization: OTLP_AUTH_HEADER
+                           ?? OTEL_EXPORTER_OTLP_HEADERS
+                           ?? '',
+        }
+    }),
+	instrumentations: [getNodeAutoInstrumentations()]
+});
+try{
+    sdk.start();
+    console.log("OTEL SDK Started");
+} catch (e) {
+    console.error("OTEL init failed:", e);
+}
--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		// Testing Integrated Observability
+		experimental: {
+			tracing: {
+				server: true
+			},
+			instrumentation: {
+				server: true
+			}
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
