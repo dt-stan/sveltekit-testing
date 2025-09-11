5babfab960f025245f319ee52afc649ed74c7abe
Switalski, Stanley <stanley.switalski@dynatrace.com>
2025-09-11 11:22:20 -0400

    Revert: Switch back to my package

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from 'amplify-adapter';
+import adapter from 'dt-stan-aws-amplify-adapter';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
