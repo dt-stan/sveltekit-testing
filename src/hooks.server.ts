d7d0262a3b0b683564a3b9ba88b8adf47ce85681
Switalski, Stanley <stanley.switalski@dynatrace.com>
2025-09-23 12:15:25 -0400

    Feat: Attempting to add Span Enrichment for User Session to Span correlation.

---
+++ src/hooks.server.ts
@@ -0,0 +1,45 @@
+// src/hooks.server.ts
+import type { Handle } from '@sveltejs/kit';
+import { context, trace, SpanKind } from '@opentelemetry/api';
+
+const tracer = trace.getTracer('sveltekit-hooks');
+
+export const handle: Handle = async ({ event, resolve }) => {
+  // Try to get the current active span
+  let span = trace.getSpan(context.active());
+  let createdNewSpan = false;
+
+  if (!span){
+    // No active span found, create a new one
+    span = tracer.startSpan('sveltekit-hooks', {
+        kind: SpanKind.INTERNAL
+    });
+    createdNewSpan = true;
+  }
+
+
+  try {
+    span.setAttribute('test', "foobar");
+    // Access the Dynatrace dtCookie to extract a session attribute
+    const rawCookie = event.cookies.get("dtCookie") ?? "";
+
+    const match = rawCookie.match(/_sn_([A-Z0-9]+)_/);
+    const sessionId: string = match?.[1] ?? "";
+
+    console.log("Extracted session ID:", sessionId);
+
+    span.setAttribute("session_id", sessionId);
+
+    // Proceed with the request
+    const response = await resolve(event);
+
+    return response;
+  } catch (error) {
+    span.recordException(error);
+    throw error;
+  } finally {
+    if (createdNewSpan) {
+        span.end();
+    }
+  }
+};
