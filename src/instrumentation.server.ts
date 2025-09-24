4ab912dc34b20f1f582a50e16969887eaf80f932
Switalski, Stanley <stanley.switalski@dynatrace.com>
2025-09-24 09:06:46 -0400

    fix: Enable VERBOSE logging

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -10,7 +10,7 @@ import { createAddHookMessageChannel } from 'import-in-the-middle';
 import { register } from 'module';
 
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.VERBOSE);
 
 // Francois Commit
 import {
