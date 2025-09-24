ab3d5a6af2ea43428fe42f7c46cd3aeba9aa309c
Switalski, Stanley <stanley.switalski@dynatrace.com>
2025-09-24 18:11:33 -0400

    debug: remove SIGTERM catch

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -78,9 +78,9 @@ try{
     console.error("OTEL init failed:", e);
 }
 
-process.on('SIGTERM', () => {
-  sdk.shutdown()
-    .then(() => console.log('OpenTelemetry SDK terminated'))
-    .catch((error) => console.error('Error terminating OpenTelemetry SDK', error))
-    .finally(() => process.exit(0));
-});
+// process.on('SIGTERM', () => {
+//   sdk.shutdown()
+//     .then(() => console.log('OpenTelemetry SDK terminated'))
+//     .catch((error) => console.error('Error terminating OpenTelemetry SDK', error))
+//     .finally(() => process.exit(0));
+// });
