d836434cca41eec9d82b3daf17a035729baa0759
Switalski, Stanley <stanley.switalski@dynatrace.com>
2025-09-24 10:00:06 -0400

    debug: add SDK shutdown to try and flush spans

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -75,3 +75,10 @@ try{
 } catch (e) {
     console.error("OTEL init failed:", e);
 }
+
+process.on('SIGTERM', () => {
+  sdk.shutdown()
+    .then(() => console.log('OpenTelemetry SDK terminated'))
+    .catch((error) => console.error('Error terminating OpenTelemetry SDK', error))
+    .finally(() => process.exit(0));
+});
