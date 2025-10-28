41b25ce9219ff8427c51b1028c013ec493a6ed66
Switalski, Stanley <stanley.switalski@dynatrace.com>
2025-10-28 16:01:07 -0400

    feat: Attempting to deploy OneAgent

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -1,94 +1,16 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
-import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
-import { resourceFromAttributes } from '@opentelemetry/resources';
-import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
-import { ConsoleInstrumentation } from '@sovarto/opentelemetry-instrumentation-console';
-import { BatchLogRecordProcessor } from '@opentelemetry/sdk-logs';
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { createAddHookMessageChannel } from 'import-in-the-middle';
-import { register } from 'module';
-
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.WARN);
-
-// Francois Commit
 import {
-    OTEL_SERVICE_NAME,
-    OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
-    OTEL_EXPORTER_OTLP_LOGS_ENDPOINT,
-    OTEL_EXPORTER_OTLP_ENDPOINT,
-    OTLP_AUTH_HEADER,
-    OTEL_EXPORTER_OTLP_HEADERS,
-    OTEL_DIAGNOSTICS
+    DT_TENANT,
+    DT_API_TOKEN
 } from '$env/static/private';
 
-const { registerOptions } = createAddHookMessageChannel();
-register('import-in-the-middle/hook.mjs', import.meta.url, registerOptions);
-
-console.log("--------------------------------");
-console.log({
-OTEL_SERVICE_NAME,
-OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
-OTEL_EXPORTER_OTLP_ENDPOINT,
-OTLP_AUTH_HEADER,
-OTEL_EXPORTER_OTLP_HEADERS,
-OTEL_DIAGNOSTICS
-});
-console.log("--------------------------------");
-
-const resource = resourceFromAttributes({
-    [ATTR_SERVICE_NAME]: OTEL_SERVICE_NAME ?? 'sveltekit-testing-intobs'
-});
-
-const traceExporter = new OTLPTraceExporter({
-        // e.g. https://your-otlp.example.com/v1/traces
-        url: OTEL_EXPORTER_OTLP_TRACES_ENDPOINT
-             ?? OTEL_EXPORTER_OTLP_ENDPOINT
-             ?? undefined,
-        headers: {
-            Authorization: OTLP_AUTH_HEADER
-                           ?? OTEL_EXPORTER_OTLP_HEADERS
-                           ?? '',
-        },
-        // timeoutMillis: 10
+(async () => {
+  try {
+    const oneAgent = await import('@dynatrace/oneagent');
+    oneAgent.default({
+        environmentid: DT_TENANT,
+        apitoken: DT_API_TOKEN
     });
-
-export const simpleSpanProcessor = new SimpleSpanProcessor(traceExporter);
-
-// const logExporter = new OTLPLogExporter({
-//     url: OTEL_EXPORTER_OTLP_LOGS_ENDPOINT
-//          ?? OTEL_EXPORTER_OTLP_ENDPOINT
-//          ?? undefined,
-//     headers: {
-//         Authorization: OTLP_AUTH_HEADER
-//                         ?? OTEL_EXPORTER_OTLP_HEADERS
-//                         ?? '',
-//     },
-//     // timeoutMillis: 10
-// });
-
-// export const batchLogProcessor = new BatchLogRecordProcessor(logExporter);
-
-const sdk = new NodeSDK({
-	resource,
-    spanProcessor: simpleSpanProcessor,
-    // logRecordProcessor: batchLogProcessor,
-	instrumentations: [getNodeAutoInstrumentations(), new ConsoleInstrumentation()]
-});
-
-try{
-    sdk.start();
-    console.log("OTEL SDK Started");
-} catch (e) {
-    console.error("OTEL init failed:", e);
-}
-
-process.on('SIGTERM', () => {
-//   batchSpanProcessor.forceFlush();
-//   console.log('Spans Flushed');
-//   batchLogProcessor.forceFlush();
-//   console.log('Logs Flushed');
-  process.exit(0);
-});
+  } catch (err) {
+    console.log('Failed to load OneAgent: ', err);
+  }
+})();
